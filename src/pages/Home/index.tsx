f460b9f8df237143cde94b2aabf107f4889195c9
vanndxh <wangyuezhou.wyz@digital-engine.com>
2022-06-25 00:02:18 +0800

    feat: css module config

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.less";
+
+function Home() {
+  return <div className={styles.text}>index</div>;
+}
+
+export default Home;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+/** css module config */
+declare module "*.less" {
+  const content: { [className: string]: string };
+  export default content;
+}
