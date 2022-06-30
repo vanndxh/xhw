0738178e2ec1550a9664a8c03dd53f08b3e86433
vanndxh <wangyuezhou.wyz@digital-engine.com>
2022-06-30 17:42:32 +0800

    feat: finish 毛玻璃效果卡片组件

---
+++ src/components/TransparentCard/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface Props {
+  /** 内部内容 */
+  children: React.ReactNode;
+  /** 宽度 */
+  width: string;
+  /** 高度 */
+  height: string;
+  /** 背景模糊度 */
+  blurDegree?: number;
+  /** 圆角度 */
+  borderRadius?: number;
+  /**
+   * 白底透明度
+   * 0-完成透明 1-完全白底（可以当正常card用）
+   */
+  bgOpacity?: number;
+}
+
+function TransparentCard(props: Props) {
+  const { children, width, height, blurDegree, borderRadius, bgOpacity } =
+    props;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: width,
+        height: height,
+        backdropFilter: blurDegree ? `blur(${blurDegree}px)` : "blur(8px)",
+        borderRadius: borderRadius ? `${borderRadius}px` : "0",
+        background:
+          bgOpacity || bgOpacity === 0
+            ? `rgba(255, 255, 255, ${bgOpacity})`
+            : `rgba(255, 255, 255, 0.5)`,
+      }}>
+      {children}
+    </div>
+  );
+}
+
+export default TransparentCard;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,36 @@
+import { NavBar } from "antd-mobile";
 import { useNavigate } from "react-router-dom";
 import BottomBar from "../../components/BottomBar";
 import styles from "./index.less";
+import TransparentCard from "@/components/TransparentCard";
 
 function Home() {
   const navigator = useNavigate();
-  function handleClick() {
-    navigator("/mine");
-  }
+
   return (
-    <>
-      <div className={styles.text}>index</div>
-      <button onClick={handleClick}>jump to mine page</button>
+    <div className={styles["home-box"]}>
+      <NavBar back={null}>主页</NavBar>
+
+      <div className={styles["home-content"]}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "100px",
+          }}>
+          <TransparentCard
+            width="40%"
+            height="70px"
+            borderRadius={16}
+            bgOpacity={0.1}
+            blurDegree={3}>
+            1
+          </TransparentCard>
+        </div>
+      </div>
+
       <BottomBar />
-    </>
+    </div>
   );
 }
 
