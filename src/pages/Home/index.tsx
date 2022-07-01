4c5d874ffcfd356ae3152f06e2042ae5f56b757f
vanndxh <wangyuezhou.wyz@digital-engine.com>
2022-07-01 13:05:51 +0800

    feat: update home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,34 @@
-import { NavBar } from "antd-mobile";
-import { useNavigate } from "react-router-dom";
-import BottomBar from "../../components/BottomBar";
-import styles from "./index.less";
+import { Grid, NavBar } from "antd-mobile";
+import BottomBar from "@/components/BottomBar";
 import TransparentCard from "@/components/TransparentCard";
+import { webData } from "./constants";
+import styles from "./index.less";
 
 function Home() {
-  const navigator = useNavigate();
-
   return (
-    <div className={styles["home-box"]}>
-      <NavBar back={null}>主页</NavBar>
+    <div className={styles["home"]}>
+      <div className={styles["home-top"]}>
+        <NavBar back={null}>主页</NavBar>
+      </div>
 
       <div className={styles["home-content"]}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingTop: "100px",
-          }}>
-          <TransparentCard
-            width="40%"
-            height="70px"
-            borderRadius={16}
-            bgOpacity={0.1}
-            blurDegree={3}>
-            1
-          </TransparentCard>
-        </div>
+        <Grid columns={2} gap={20}>
+          {webData.gameWeb.map((i) => {
+            return (
+              <Grid.Item
+                onClick={() => {
+                  window.open(i.url);
+                }}>
+                <TransparentCard borderRadius={16} bgOpacity={0.3}>
+                  <div className={styles["web-card"]}>
+                    <p className={styles["web-card-title"]}>{i.title}</p>
+                    <p className={styles["web-card-desc"]}>{i.desc}</p>
+                  </div>
+                </TransparentCard>
+              </Grid.Item>
+            );
+          })}
+        </Grid>
       </div>
 
       <BottomBar />
