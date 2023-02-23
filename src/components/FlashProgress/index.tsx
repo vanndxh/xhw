629eb8e2ce1805fec5b8076b3f758a13464c621d
vanndxh <1025196468@qq.com>
2023-02-23 20:55:08 +0800

    feat: add FlashProgress

---
+++ src/components/FlashProgress/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./index.module.less";
+
+interface Props {
+  /** 进度条百分比 */
+  percent: number;
+  /** 背景色 */
+  containerColor?: string;
+  /** 填充色 */
+  fillColor?: string;
+  /** 粗细 */
+  thick?: string;
+}
+
+function FlashProgress(props: Props) {
+  const {
+    percent,
+    containerColor = "rgba(255, 255, 255, 0.4)",
+    fillColor = "yellow",
+    thick = "5px",
+  } = props;
+
+  return (
+    <div
+      className={styles["progress-container"]}
+      style={{ background: containerColor, height: thick }}
+    >
+      <div
+        className={styles["progress-fill"]}
+        style={{
+          width: `${percent}%`,
+          background: fillColor,
+          height: thick,
+        }}
+      />
+    </div>
+  );
+}
+export default FlashProgress;
--- src/pages/Workspace/Pages/ComponentTest/index.tsx
+++ src/pages/Workspace/Pages/ComponentTest/index.tsx
@@ -1,15 +1,31 @@
 /**
  * @file 组件测试页
  */
-import React from "react";
+import React, { useState } from "react";
 import AutoPrint from "@/components/AutoPrint";
 import styles from "./index.module.less";
+import FlashProgress from "@/components/FlashProgress";
+import { Button } from "antd";
 
-function componentTest() {
+function ComponentTest() {
+  const [percent, setPercent] = useState(50);
+
   return (
     <div className={styles["component"]}>
       <AutoPrint text={"这是一段测试文本"} />
+
+      <div style={{ width: 200 }}>
+        <FlashProgress percent={percent} fillColor="rgb(254, 224, 120)" />
+      </div>
+
+      <Button
+        onClick={() => {
+          setPercent((pre) => pre + 10);
+        }}
+      >
+        +10
+      </Button>
     </div>
   );
 }
-export default componentTest;
+export default ComponentTest;
