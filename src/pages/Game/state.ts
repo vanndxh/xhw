9d0bb1ecb0ee8f26029feba84c5ca90f874a95e1
vanndxh <1025196468@qq.com>
2024-05-08 22:31:13 +0800

    feat: valtio框架搭建

---
+++ src/pages/Game/state.ts
@@ -0,0 +1,60 @@
+/**
+ * @file 游戏数据全局state
+ */
+
+import { safeParse } from "@/utils/utils";
+import { RoleSourceKey } from "./constants";
+import { proxy, subscribe } from "valtio";
+
+/** 抽卡记录数据类型 */
+export type HistoryType = {
+  /** 名称 */
+  name?: string;
+  /** id */
+  id?: string;
+  /** 抽卡时间 */
+  time?: string;
+  /** 来源 */
+  source?: RoleSourceKey;
+  /** 多少抽出的 */
+  pulls?: number;
+};
+/** 用户数据类型 */
+export type UserDataType = {
+  /** 剩余总抽数 */
+  pulls?: number;
+  /** 抽卡记录 */
+  history?: HistoryType[];
+  /** 是否抽卡次数无限 */
+  infinite?: boolean;
+  /** 总抽数 */
+  pullCount?: number;
+  /** 总战斗次数 */
+  fightCount?: number;
+  /** 当前水位线 */
+  level?: number;
+};
+/** 用户数据初始化 */
+export const initUserData = {
+  pulls: 1000,
+  history: [],
+  infinite: false,
+  pullCount: 0,
+  fightCount: 0,
+  level: 0,
+};
+
+export const userData = proxy<UserDataType>(initUserData);
+
+console.log("userData", userData);
+
+/** 根据localStorage更新用户数据 */
+const storedState = safeParse(localStorage.getItem("userData"));
+if (storedState) {
+  Object.assign(userData, storedState);
+}
+
+/** 将userData保存到localStorage */
+subscribe(userData, () => {
+  localStorage.setItem("test_userData", JSON.stringify(userData));
+});
