331b1be11baf974d5a47728e843fc5a97d27e237
vanndxh <1025196468@qq.com>
2025-02-07 20:00:55 +0800

    feat: 文件结构修改

--- config/routes.tsx
+++ config/routes.tsx
@@ -4,13 +4,14 @@ import GPT from "@/pages/GPT";
 import MarkdownParse from "@/pages/MarkdownParse";
 import * as Game from "@/pages/Game";
 import Docs from "@/pages/Docs";
+import { Navigate } from "react-router-dom";
 
 export const routes = [
   {
     path: "/",
     element: <Index />,
     children: [
-      { path: "", redirect: "genshin" },
+      { path: "", element: <Navigate to="/genshin" replace /> },
       { path: "genshin", element: <Genshin /> },
       { path: "gpt", element: <GPT /> },
       { path: "markdown", element: <MarkdownParse /> },
--- src/services/api/Doc.mjs
+++ src/services/api/Doc.mjs
@@ -9,7 +9,7 @@ export const getMarkdownDocs = (req, res) => {
   const finalList = [];
   const docTitleList = ["Docker部署Go后端", "xhw-pro开发笔记", "前端面试手册", "大模型对话技术", "开发环境配置"];
   docTitleList.forEach((item) => {
-    const file = fs.readFileSync(`src\\services\\database\\docs\\${item}\\index.md`, "utf8");
+    const file = fs.readFileSync(`src\\docs\\${item}\\index.md`, "utf8");
     finalList.push({
       title: item,
       content: file,
